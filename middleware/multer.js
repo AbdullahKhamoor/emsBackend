a903ea7a5c48d982e1ef1ca3c0e94b4d81fc06ca
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-17 14:55:30 +0500

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,12 +1,8 @@
+import multer from "multer"
 
-<<<<<<< HEAD
 const storage = multer.diskStorage({})
 
-const upload = multer({
-  storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
-})
+const upload = multer({storage})
 
 export default upload
-=======
->>>>>>> a09478f94a26eac32f498da6b3822e8f85835199
+
