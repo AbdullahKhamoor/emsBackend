a09478f94a26eac32f498da6b3822e8f85835199
AbdullahKhamoor <115545624+AbdullahKhamoor@users.noreply.github.com>
2026-01-17 11:53:43 +0500

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,10 +1,1 @@
-import multer from "multer"
 
-const storage = multer.diskStorage({})
-
-const upload = multer({
-  storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
-})
-
-export default upload
