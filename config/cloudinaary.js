f63f817411aca69f3fa87f85e78c3837908cd13e
Abdullah <abdullah.khamoor4@gmail.com>
2026-01-17 17:26:54 +0500

    update

--- config/cloudinaary.js
+++ config/cloudinaary.js
@@ -1,6 +1,6 @@
 import cloudinaary from "cloudinary";
 
-cloudinary.v2.config({
+cloudinaary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  // tumhara cloud name
   api_key: process.env.CLOUDINARY_API_KEY,        // tumhara API key
   api_secret: process.env.CLOUDINARY_API_SECRET,  // tumhara API secret
